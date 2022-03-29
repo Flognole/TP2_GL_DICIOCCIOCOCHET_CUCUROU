d83dcb36e42319f8f400aa0c4b2ab6dd17cfa6f1
Cucurou Florent <flochel2102@gmail.com>
2022-03-29 10:09:02 +0200

    notre dossier

--- System_hospitalier.java
+++ System_hospitalier.java
@@ -1,3 +1,4 @@
+
 package tp_git;
 
 import java.util.ArrayList;
@@ -34,12 +35,12 @@ public class System_hospitalier {
 	}
 	
 	private String analyserRequete(Requête requête) {
-		// Analyser la requête
+		// Analyser la requête prend en parametre un objet de la classe requete et retourne que la requete est analysee
 		return "requête analysée";
 	}
 	
 	private String comparerDonnes() {
-		// Comparaison des données
+		// Comparaison des données sert a comparer les donnees dans le but de trouver si oui ou non il y a un cluster
 		return "Cluster trouvé";
 	}
 
